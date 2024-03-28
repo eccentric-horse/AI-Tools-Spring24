9674f3def9cb123aa2ec625556b97ba9a5e17ebd
eccentric-horse <shuxu.huffman@gallaudet.edu>
2024-03-28 11:03:11 -0700

    add week 11

---
+++ week11/vectordb_signlanguages.py
@@ -0,0 +1,26 @@
+import chromadb
+from chromadb.utils import embedding_functions
+import os
+
+# Create a new database client
+# We want the data to be persisted
+client = chromadb.PersistentClient(path="signlanguagesdb")
+
+collection = client.create_collection(
+    'signlanguages',
+    embedding_function=embedding_functions.OpenAIEmbeddingFunction(
+        api_key=os.environ['OPENAI_API_KEY'],
+        model_name="text-embedding-ada-002"
+    ))
+
+filepath = '../signlanguages'
+
+for f in os.listdir(filepath):
+    path = os.path.join(filepath, f)
+    with open(path, 'r', encoding="utf8", errors='ignore') as f:
+        text = f.read()
+
+    text = text.replace('\n', ' ')
+
+    collection.add(documents=[text], ids=[path])
+
