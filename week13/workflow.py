11954d9296968194c282c75621571971e8fceb2b
eccentric-horse <shuxu.huffman@gallaudet.edu>
2024-04-17 12:42:42 -0700

    add week 14 materials

--- week13/workflow.py
+++ week13/workflow.py
@@ -8,7 +8,7 @@ from utilities import ChatTemplate
 import json
 
 response = ChatTemplate({
-    'messages': [{'role': 'user', 'content': 'Your goal is to write a short 3 chapter sci-fi novella on the theme of AI ethics. Come up with 7 steps that will help you achieve the goal. Output these steps as a JSON array of large language model prompts.'}]})
+    'messages': [{'role': 'user', 'content': 'Your goal is to write a short 3 chapter sci-fi novella on the theme of AI ethics. Come up with 7 steps that will help you achieve the goal. Output these steps as a JSON array of large language model prompts. Note: you need to give exactly 7 steps. Each of them needs to start with "Step number:". For example: "Step 1: brainstorm the main characters and the plot".'}]})
 
 response_json = response.completion({}).choices[0].message.content
 
@@ -24,7 +24,7 @@ response_json = response_json.replace('json\n', '').replace('\n', '')
 plan = json.loads(response_json)
 
 chat = ChatTemplate({
-    'messages': [{'role': 'user', 'content': 'Write a short 3 chapter sci-fi novella on the theme of AI ethics.'}]})
+    'messages': [{'role': 'system', 'content': 'Write a short 3 chapter sci-fi novella on the theme of AI ethics.'}]})
 
 for step in plan:
     chat.template['messages'].append({'role': 'user', 'content': step})
